300a7b6f9f4404ade91798b8cee3522edae456a4
ray2078 <77797345+free2swap@users.noreply.github.com>
2023-01-24 21:38:25 +0800

    Update server url to main branch

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-w9x5.onrender.com/', {
+    const response = await fetch('https://codex-w9x5.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
